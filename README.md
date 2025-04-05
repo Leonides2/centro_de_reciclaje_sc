ff4ed07d73113bb480217797cc12a15350b9240c
José Leonardo Zúñiga Hernández <114005429+Leonides2@users.noreply.github.com>
2025-04-05 14:12:17 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# centro_de_reciclaje_sc
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/centro_de_reciclaje_sc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.centro_de_reciclaje_sc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
