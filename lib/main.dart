01b8d96986cf8b229102152ce444641e800ad5c7
juanjosecorella <juanjosecorella2004@gmail.com>
2025-05-18 18:04:30 -0600

    feat: bottom navbar prototype

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,56 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      home: Scaffold(body: HomePage()),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color(0x017d1c),
+          brightness: Brightness.light,
         ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePage();
+}
+
+class _HomePage extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int i) {
+    setState(() {
+      _selectedIndex = i;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Center(child: Text("Centro de Reciclaje SC"))),
+      body: Center(child: Text("${_selectedIndex}")),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_add),
+            label: 'Usuarios',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.work), label: 'Materiales'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.description),
+            label: 'Historial',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Perfil'),
+        ],
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
